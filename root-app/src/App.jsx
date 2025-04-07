b9088cf36546ac08f194780ee62fcac98c79b0e8
rebeccaaaaOrram <rebecca.ignacio@orram.com.br>
2025-04-07 16:23:35 -0300

    refactor: discommented login component in root app

--- root-app/src/App.jsx
+++ root-app/src/App.jsx
@@ -2,14 +2,14 @@ import React, { Suspense } from "react";
 import { MantineProvider, TextInput } from "@mantine/core";
 import '@mantine/core/styles.css';
 
-// const Login = React.lazy(() => import("login_app/Login"));
+const Login = React.lazy(() => import("login_app/Login"));
 
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      {/* <Suspense fallback={<div>Carregando Login...</div>}>
+     <Suspense fallback={<div>Carregando Login...</div>}>
         <Login />
-      </Suspense> */}
+      </Suspense>
     </MantineProvider>
   );
 }
