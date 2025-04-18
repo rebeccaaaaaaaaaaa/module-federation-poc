72cb5825f011ee32205294346edece768b205f1e
rebeccaaaaOrram <rebecca.ignacio@orram.com.br>
2025-04-18 20:02:40 -0300

    feat: config to use env

--- root-app/vite.config.ts
+++ root-app/vite.config.ts
@@ -1,19 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
 
-export default defineConfig({
-  build: {
-    target: 'esnext',
-  },
-  plugins: [
-    react(),
-    federation({
-      name: 'root_app',
-      remotes: {
-        header_app: 'http://localhost:4174/assets/remoteEntry.js',
-      },
-      shared: ['react', 'react-dom'],
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const headerAppUrl = env.VITE_ENTRY_HEADER_APP_URL;
+
+  if (!headerAppUrl) {
+    throw new Error('VITE_ENTRY_HEADER_APP_URL não está definido');
+  }
+
+  return {
+    build: {
+      target: 'esnext',
+    },
+    plugins: [
+      react(),
+      federation({
+        name: 'root_app',
+        remotes: {
+          header_app: headerAppUrl,
+        },
+        shared: ['react', 'react-dom'],
+      }),
+    ],
+  };
 });
