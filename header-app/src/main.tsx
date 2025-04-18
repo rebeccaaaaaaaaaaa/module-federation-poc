49ff8b0eb4b750458c53d548c1ea5e4d58fd0417
rebeccaaaaOrram <rebecca.ignacio@orram.com.br>
2025-04-18 17:03:37 -0300

    fix: fix error after merge

--- header-app/src/main.tsx
+++ header-app/src/main.tsx
@@ -1,18 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 export {}
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 1a024e4 (chore: create a boiler plate)
-=======
-export {}
->>>>>>> 5765ca5 (feat: create first expose and fix all errors about this)
