77c83eba60452bf3a03655e56998326fd5a74797
rebeccaaaaOrram <rebecca.ignacio@orram.com.br>
2025-05-12 21:50:17 -0300

    Revert "refactor: change .env values to load in local"
    This reverts commit 80d3212dbcce4671b822c6e1575487a636224fbb.

--- login-app/vite.config.ts
+++ login-app/vite.config.ts
@@ -5,6 +5,7 @@ import federation from "@originjs/vite-plugin-federation";
 export default defineConfig({
   server: {
     port: 4175,
+    open: true,
     strictPort: true,
     cors: {
       origin: "*",
